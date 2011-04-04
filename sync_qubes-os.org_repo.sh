1ab8a7f89b0eb5ec9b7f45f6f2b954c22d2f97d8
Joanna Rutkowska <joanna@invisiblethingslab.com>
2011-04-04 10:53:11 +0200

    Introduce current-release symlink for use in other Makefiles

--- sync_qubes-os.org_repo.sh
+++ sync_qubes-os.org_repo.sh
@@ -5,5 +5,5 @@ HOST=yum.qubes-os.org
 
 
 echo "Syncing repos to remote host $HOST..."
-rsync $DRY --delete --partial --progress --exclude=*.sh -air * joanna@$HOST:/var/www/yum.qubes-os.org/
+rsync $DRY --delete --partial --progress --exclude=*.sh --exclude=current-release -air * joanna@$HOST:/var/www/yum.qubes-os.org/
 
