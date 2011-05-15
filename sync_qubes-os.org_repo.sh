bec6dab5d80e31f78e340ae5262f5302a34cc2b7
Joanna Rutkowska <joanna@invisiblethingslab.com>
2011-05-15 16:14:48 +0200

    rsync each repo separately

--- sync_qubes-os.org_repo.sh
+++ sync_qubes-os.org_repo.sh
@@ -5,5 +5,6 @@ HOST=yum.qubes-os.org
 
 
 echo "Syncing repos to remote host $HOST..."
-rsync $DRY --delete --partial --progress --exclude=*.sh --exclude=current-release -air * joanna@$HOST:/var/www/yum.qubes-os.org/
+rsync $DRY --delete --partial --progress -air r1-beta1/current/* joanna@$HOST:/var/www/yum.qubes-os.org/r1-beta1/current/
+rsync $DRY --delete --partial --progress -air r1-beta1/current-testing/* joanna@$HOST:/var/www/yum.qubes-os.org/r1-beta1/current-testing/
 
