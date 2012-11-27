f4caea58e9fe647f449500033fe9b9085318f8f0
Joanna Rutkowska <joanna@invisiblethingslab.com>
2012-11-27 16:12:40 +0100

    sync_qubes-os.org_repo.sh: sync only r2 release, and also remove the deadly --delete switch for rsync

--- sync_qubes-os.org_repo.sh
+++ sync_qubes-os.org_repo.sh
@@ -2,7 +2,7 @@
 
 #DRY="-n"
 HOST=yum.qubes-os.org
-RELS_TO_SYNC="r1 r2"
+RELS_TO_SYNC="r2"
 REPOS_TO_SYNC="current current-testing"
 
 for rel in $RELS_TO_SYNC; do
@@ -10,7 +10,7 @@ for rel in $RELS_TO_SYNC; do
     for repo in $REPOS_TO_SYNC; do
         echo
         echo "Syncing $rel/$repo..."
-        rsync $DRY --delete --partial --progress -air $rel/$repo/* joanna@$HOST:/var/www/yum.qubes-os.org/$rel/$repo/
+        rsync $DRY --partial --progress -air $rel/$repo/* joanna@$HOST:/var/www/yum.qubes-os.org/$rel/$repo/
     done
 
 done
