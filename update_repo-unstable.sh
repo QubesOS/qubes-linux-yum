ab8faff3a4ce6de8d3ebba063f28f6f9568f00e2
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2014-05-30 23:34:39 +0200

    Add AUTOMATIC_UPLOAD option also to unstable repo scripts

--- update_repo-unstable.sh
+++ update_repo-unstable.sh
@@ -3,3 +3,8 @@
 REPOS_TO_UPDATE="current-release/unstable/dom0/fc* current-release/unstable/vm/*"
 
 . `dirname $0`/update_repo.sh
+
+if [ "$AUTOMATIC_UPLOAD" = 1 ]; then
+    `dirname $0`/sync_qubes-os.org_repo-unstable.sh
+fi
+
