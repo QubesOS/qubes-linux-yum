894b19280bf91d75d59a9bc84e2fa95bcbc22b08
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2015-04-29 01:47:10 +0200

    Remove explicit username set
    It is much better to user default - can be easily configured for
    example as this:
    cat ~/.ssh/config
    Host yum.qubes-os.org
            User marmarek

--- sync_qubes-os.org_repo-unstable.sh
+++ sync_qubes-os.org_repo-unstable.sh
@@ -3,7 +3,6 @@
 pushd `dirname $0`
 
 #DRY="-n"
-USERNAME=marmarek
 HOST=yum.qubes-os.org
 if [ -n "$1" ]; then
     RELS_TO_SYNC=`basename "$1"`
@@ -17,9 +16,9 @@ for rel in $RELS_TO_SYNC; do
     for repo in $REPOS_TO_SYNC; do
         echo
         echo "Syncing $rel/$repo..."
-        rsync $DRY --partial --progress --exclude repodata -air $rel/$repo/* $USERNAME@$HOST:/var/www/yum.qubes-os.org/$rel/$repo/
-        rsync $DRY update_repo.sh update_repo-arg.sh $USERNAME@$HOST:
-        [ -z "$DRY" ] && ssh $USERNAME@$HOST ./update_repo-arg.sh "/var/www/yum.qubes-os.org/$rel/$repo/dom0/fc*" "/var/www/yum.qubes-os.org/$rel/$repo/vm/fc*"
+        rsync $DRY --partial --progress --exclude repodata -air $rel/$repo/* $HOST:/var/www/yum.qubes-os.org/$rel/$repo/
+        rsync $DRY update_repo.sh update_repo-arg.sh $HOST:
+        [ -z "$DRY" ] && ssh $HOST ./update_repo-arg.sh "/var/www/yum.qubes-os.org/$rel/$repo/dom0/fc*" "/var/www/yum.qubes-os.org/$rel/$repo/vm/fc*"
     done
 
 done
