32b91123f851fd771983bbd4ca5f03ec39c5af12
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2015-08-09 19:41:32 +0200

    No longer use sf.net for template rpms

---
+++ sync_qubes-os.org_repo-templates.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+pushd `dirname $0`
+
+usage() {
+    echo "Usage: $0 <RELEASE> <REPOS>"
+    echo "<RELEASE>: r2, r3.0"
+    echo "<REPOS>: templates-community, templates-itl"
+    exit 1
+}
+
+#DRY="-n"
+HOST=yum.qubes-os.org
+HOST_BASEDIR=/pub/qubes/repo/yum
+if [ -n "$1" ]; then
+    RELS_TO_SYNC=`basename "$1"`
+    shift
+else
+    usage
+fi
+REPOS_TO_SYNC="$*"
+
+if [ -z "$REPOS_TO_SYNC" ]; then
+    usage
+fi
+
+for rel in $RELS_TO_SYNC; do
+
+    rsync_args=
+    for repo in $REPOS_TO_SYNC; do
+        rsync_args="$rsync_args $rel/$repo"
+    done
+    echo "Syncing $rel..."
+    rsync $DRY --partial --progress --hard-links --exclude repodata -air $rsync_args $HOST:$HOST_BASEDIR/$rel/
+    rsync $DRY update_repo.sh update_repo-arg.sh $HOST:
+    for repo in $REPOS_TO_SYNC; do
+        [ -z "$DRY" ] && ssh $HOST ./update_repo-arg.sh "$HOST_BASEDIR/$rel/$repo"
+    done
+
+done
+
+popd
--- update_repo-template.sh
+++ update_repo-template.sh
@@ -1,53 +1,5 @@
 #!/bin/sh
 
-if [ -n "$1" ]; then
-    current_release=$1
-else
-    current_release=current-release
-fi
+REPOS_TO_UPDATE="$1/templates-itl $1/templates-community"
 
-if [ -z "$REPOS_TO_UPDATE" ]; then
-    REPOS_TO_UPDATE="$current_release/templates-itl $current_release/templates-community"
-fi
-
-TEMPLATES_BASEURL="http://sourceforge.net/projects/qubesos/files/rpm/$current_release"
-
-# $1 -- path to rpm dir
-check_repo()
-{
-    if [ "x$SKIP_REPO_CHECK" = "x1" ]; then
-        return 0
-    fi
-    if rpm --checksig $1/*.rpm | grep -v pgp > /dev/null ; then
-        echo "ERROR: There are unsigned RPM packages in $1 repo:"
-        echo "---------------------------------------"
-        rpm --checksig $1/*.rpm | grep -v pgp 
-        echo "---------------------------------------"
-        echo "Sign them before proceeding."
-        exit 1
-    fi
-}
-
-
-update_repo()
-{
-    OPTS=
-    [ -r "$1/comps.xml" ] && OPTS="-g comps.xml"
-    createrepo $OPTS --update -u "$TEMPLATES_BASEURL/`basename $1`" -o $1 $1 >/dev/null
-}
-
-is_repo_empty() {
-    ls $1/*.rpm > /dev/null 2>&1 || return 0
-    return 1
-}
-
-for repo in $REPOS_TO_UPDATE ; do
-    echo "--> Processing repo: $repo..."
-    if ! is_repo_empty $repo ; then
-        check_repo $repo || exit 1
-    fi
-    update_repo $repo || exit 1
-done
-echo Done.
-
-#yum clean metadata
+. `dirname $0`/update_repo.sh
