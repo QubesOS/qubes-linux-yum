9f529a548c2551d3e0b5fa871bb7aa40a702c368
Frédéric Pierret <frederic.epitre@orange.fr>
2017-10-28 07:22:31 -0400

    Add support for templates upload and syncing

--- update_repo-template.sh
+++ update_repo-template.sh
@@ -3,3 +3,7 @@
 REPOS_TO_UPDATE="$1/templates-itl $1/templates-community"
 
 . `dirname $0`/update_repo.sh
+
+if [ "$AUTOMATIC_UPLOAD" = 1 ]; then
+    `dirname $0`/sync_qubes-os.org_repo-templates.sh "$1" "templates-itl" "templates-community"
+fi
