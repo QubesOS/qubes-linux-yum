b582abe46893b2f06688e000656ba12d48cf86e2
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2018-07-12 22:57:03 +0200

    Automatically determine templates repos to upload
    Don't hardcode the list to templates-itl,templates-community regardless
    of what was actually updated.

--- update_repo-template.sh
+++ update_repo-template.sh
@@ -12,5 +12,5 @@ fi
 . `dirname $0`/update_repo.sh
 
 if [ "$AUTOMATIC_UPLOAD" = 1 ]; then
-    `dirname $0`/sync_qubes-os.org_repo-templates.sh "$1" "templates-itl" "templates-community"
+    `dirname $0`/sync_qubes-os.org_repo-templates.sh "$1" $(basename -a $REPOS_TO_UPDATE)
 fi
